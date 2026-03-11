493e04155a96c0956a1711611b9ff31beca0ee24
fabian-tu <fabian.tuertscher@students.fhv.at>
2026-03-11 09:35:08 +0100

    Add endpoints and tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,12 +19,18 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-restclient")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testImplementation("org.springframework.boot:spring-boot-starter-restclient-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+
+	testLogging {
+        events("passed", "skipped", "failed")
+    }
 }
---
+++ src/main/java/com/example/demo/TodoController.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/todos")
+public class TodoController {
+
+    private final List<Todo> todos = new ArrayList<>();
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping
+    public List<Todo> getAllTodos() {
+        return todos;
+    }
+
+    @PostMapping
+    public Todo addTodo(@RequestBody Todo todo) {
+        todo.setId(counter.incrementAndGet());
+        todos.add(todo);
+        return todo;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> removeTodo(@PathVariable Long id) {
+        boolean isRemoved = todos.removeIf(todo -> todo.getId().equals(id));
+        
+        if (isRemoved) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
